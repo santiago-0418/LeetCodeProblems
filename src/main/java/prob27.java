1fca60cf4b95280e7d0735364293c6bcc2124bb5
lince505 <linkys505@gmail.com>
2025-01-28 10:08:49 -0500

    prob27

--- src/main/java/prob27.java
+++ src/main/java/prob27.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
+
 public class prob27 {
     public static void main(String[] args) {
-
+        int[] test = new int[]{3,2,2,3};
+        System.out.println(removeElement(test ,3));
+        System.out.println(Arrays.toString(test));
     }
 
-    class Solution {
+//    static class Solution {
+        public static int removeElement(int[] nums, int val) {
+            int count = 0;
+            for(int i = 0; i < nums.length; i++){
+                if(nums[i] != val){
+                    nums[count] = nums[i];
+                    count++;
+                }
+            }
+            return count;
     }
 }
